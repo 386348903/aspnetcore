9cca44c16b97d507ca4c1eaf64438093d48e150a
386348903 <386348903@qq.com>
2017-03-07 15:53:15 +0800

    ok
    ok

---
+++ Bll/Register.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bll
+{
+    /// <summary>
+    /// 注册模块
+    /// </summary>
+    public class Register
+    {
+        /// <summary>
+        /// 添加账号
+        /// </summary>
+        /// <param name="name">用户名</param>
+        /// <param name="pwd">密码</param>
+        /// <returns></returns>
+        public static Model.Userone User_Insert(string name,string pwd)
+        {
+            Model.Userone user = new Model.Userone() { name = name, pwd = Gx.DataCrypt.MD5(pwd), role = "用户", time = DateTime.Now };
+            //唯一性检验
+            if (Dal.Userone.Exists_Register(user))
+            {
+                return null;
+            }
+            if(!Dal.Userone.Insert(user))
+            {
+                return null;
+            }
+            return user;
+        }
+    }
+}
---
+++ Bll/shengpi.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bll
+{
+    /// <summary>
+    /// 博客审批
+    /// </summary>
+    public class shengpi
+    {
+        /// <summary>
+        /// 获取列表
+        /// </summary>
+        /// <param name="type">状态</param>
+        /// <returns></returns>
+        public static List<Model.V_bk> SelectList(int type)
+        {
+            Model.V_bk bk = new Model.V_bk() { type= type };
+            return Dal.V_bk.SelectList(bk);
+        }
+        /// <summary>
+        /// 获取列表
+        /// </summary>
+        /// <param name="uid">用户id</param>
+        /// <returns></returns>
+        public static List<Model.V_bk> UserList(int uid)
+        {
+            Model.V_bk bk = new Model.V_bk() { uid = uid };
+            return Dal.V_bk.UserList(bk);
+        }
+        public static bool Insert(Model.bk bk)
+        {
+            if (Dal.bk.Exists(bk))
+                return false;
+            return Dal.bk.Insert(bk);
+        }
+        public static bool Delete(Model.bk bk)
+        {
+            return Dal.bk.Delete(bk);
+        }
+        public static bool Update(Model.bk bk)
+        {
+            return Dal.bk.Update(bk);
+        }
+    }
+}
